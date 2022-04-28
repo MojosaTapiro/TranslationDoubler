8e92a2ce180d13c2949cbadfa5f43ed67bb4ee8c
MojosaTapiro <tabea.maltry@yahoo.com>
2022-04-28 17:06:01 +0200

    Initial commit

---
+++ Quiplash_translationDoubler.py
@@ -0,0 +1,20 @@
+#! python3
+# finds corresponding data.jet file in QuiplashQuestion folder for each prompt in QuiplashQuestion.jet and replaces untranslated prompt in data.jet with translation from QuiplashQuestion.jet
+import os, json
+os.chdir(os.path.realpath(__file__).replace('translationDoubler.py', ''))
+
+questionsMain = json.load(open(os.path.join('Quiplash', 'content', 'QuiplashQuestion.jet'), 'r', encoding='utf-8'))
+output = {}
+for i in range(len(questionsMain['content'])):
+    textID = str(questionsMain['content'][i]["id"])
+    data = json.load(open(os.path.join('Quiplash', 'content', 'QuiplashQuestion', textID, 'data.jet'), 'r', encoding='utf-8'))
+    translatedText = questionsMain['content'][i]["prompt"]
+    untranslatedText = data["fields"][4]["v"]
+    data["fields"][4]["v"] = translatedText
+    writer = open(os.path.join('Quiplash', 'content', 'QuiplashQuestion', textID, 'data.jet'), 'w', encoding='utf-8')
+    json.dump(data, writer)
+
+    
+    # if translatedText != data["fields"][4]["v"]:
+    #     print(data["fields"][4]["v"])
+
